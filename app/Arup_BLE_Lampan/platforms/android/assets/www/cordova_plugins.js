c38d34e9dca200bb70b4bca82ca2c711689d7de0
Francesco Anselmo <francesco.anselmo@gmail.com>
2015-08-23 23:41:23 +0100

    Now including files for Google Play app store upload

--- app/Arup_BLE_Lampan/platforms/android/assets/www/cordova_plugins.js
+++ app/Arup_BLE_Lampan/platforms/android/assets/www/cordova_plugins.js
@@ -19,7 +19,6 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "org.apache.cordova.device": "0.3.0",
-    "org.apache.cordova.console": "0.2.13",
     "com.evothings.ble": "0.0.1"
 }
 // BOTTOM OF METADATA
--- app/Arup_BLE_Lampan/platforms/ios/www/cordova_plugins.js
+++ app/Arup_BLE_Lampan/platforms/ios/www/cordova_plugins.js
@@ -8,20 +8,6 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
-        "id": "org.apache.cordova.console.console",
-        "clobbers": [
-            "console"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.console/www/logger.js",
-        "id": "org.apache.cordova.console.logger",
-        "clobbers": [
-            "cordova.logger"
-        ]
-    },
-    {
         "file": "plugins/com.evothings.ble/ble.js",
         "id": "com.evothings.ble.BLE",
         "clobbers": [
@@ -33,7 +19,6 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "org.apache.cordova.device": "0.3.0",
-    "org.apache.cordova.console": "0.2.13",
     "com.evothings.ble": "0.0.1"
 }
 // BOTTOM OF METADATA
